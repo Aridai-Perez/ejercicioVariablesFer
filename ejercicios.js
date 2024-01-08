5df7fa9b0e1187a31ba45cffda87d8f0d492e683
Yithzabi Aridai F. Perez <141900042+Aridai-Perez@users.noreply.github.com>
2024-01-08 15:16:19 -0800

    Add files via upload

---
+++ ejercicios.js
@@ -0,0 +1,105 @@
+/** Ejercicio para esta sesion
+ * 
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+ *      -number
+ *      -string
+ *      -boolean
+ * 
+ * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+ *      -suma
+ *      -resta
+ *      -multiplicacion
+ *      -division
+ * 
+ * Declarar variables que concatenen cadenas de texto
+ *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+ * 
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ * 
+ *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ * Imprimir los resultados en pantalla con un console.log()
+ */
+
+//Variables de cada tipo
+
+//String
+let nombreAri = "Aridai";
+let apellidoAri = "Perez";
+let peliFavAri = "Frozen 2";
+let papitasFavsAri = "Chips con chamoy";
+let cancionFavAri = "A Nonsense Christmas By Sabrina Carpenter"
+let cantanteFavAri = "Sabrina Carpenter"
+let comidaFavAri = "Tacos de adobada"
+let componenteDePc = "procesador"
+let lenguajeFavAri = "JavaScript"
+let areaDevAri = "Frontd"
+
+//Number
+let edadAri = 20;
+let numMascotasAri = 8;
+let numHermanosAri = 1;
+let diasDeSemana = 7;
+let monthsOfYear = 12;
+let mentoresTecnicosGen = 2;
+let mentorasHGen = 2;
+let horasDeldia = 24;
+let diasDeEnero = 31;
+let diaCumpleAri = 15;
+
+//Boolean
+let ariViveSola = false;
+let mayorDeEdad = true;
+let esLunes = true;
+let isALive = true;
+let isARobot = false;
+let isRaining = false;
+let iphone = true;
+let itsCold = true;
+let twoMimir = true;
+let soporto = false;
+
+
+// Variables numericas
+
+let numero1 = 45;
+let numero2 = 27;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Partiendo de los numeros; " + numero1 + " y " + numero2 + ". Los resultados son:");
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicacion: " + multiplicacion);
+console.log("Division: " + division);
+
+//Mensajes que concatenen
+
+let nombreJuan = "Juan";
+let diaDeLaSemana = "Jueves";
+let comidaFavorita = "Pozole";
+let estadoNatal = "Baja california";
+let cantanteFavorito = "Humbe";
+
+console.log("Buenos dias " + nombreJuan + ", como te encuentras?")
+console.log("Como crees que este el clima el " + diaDeLaSemana + "?")
+console.log("Yo comi " + comidaFavorita + " en navidad, tu tambien? woow")
+console.log("que tal, yo naci en " + estadoNatal +", y tu?")
+console.log("A ti tambien te gusta " + cantanteFavorito + "! Es mi cantante favorito.")
+
+//Conversion
+
+let edadPancho = 30;
+let edadJuana = "25";
+let saludo = "Hola";
+let numeroPar = "2";
+let numeroImpar = 3;
+
+let conversionString1 = String(edadPancho);
+let conversionNumber1 = Number(edadJuana);
+let conversionBoolean = Boolean(saludo);
+let conversionNumber2 = Number(numeroPar);
+let conversionString2 = String(numeroImpar); 
